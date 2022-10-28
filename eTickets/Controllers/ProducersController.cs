46ec6437d9474c4e71b7705b28725ce017c96957
Maciej Łącki <maciej.lacky@gmail.com>
2022-10-28 20:07:45 +0200

    views for controllers

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -17,7 +17,7 @@ namespace eTickets.Controllers
         public async Task<IActionResult> Index()
         {
             var data = await _context.Producer.ToListAsync();
-            return View();
+            return View(data);
         }
     }
 }
