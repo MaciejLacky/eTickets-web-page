d07b50b77b4e94d9f711842359267e4a2e99bc5c
Maciej Łącki <maciej.lacky@gmail.com>
2022-10-30 19:56:17 +0100

    add services actors

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -8,10 +8,13 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage ="Full Name is required")]
         public string FullName { get; set; }
         [Display(Name ="Picture")]
+        [Required(ErrorMessage = "Picture is required")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Bio is required")]
         public string Bio { get; set; }
         public List<Actor_Movie> Actors_Movie { get; set; }
     }
